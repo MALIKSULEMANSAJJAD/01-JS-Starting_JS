aa479c81c4ad93193c60891c5f95bb692b0dbebf
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-20 00:55:04 +0500

    3rd

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 console.log("Hellol posse");
 console.log("aight, let's do dis shish");
 console.log("");
-console.log("bs kal 2nd last paper hai, phir full nizam laga dena");
+console.log("");
 console.log("paper muk gye");
 console.log("kal se kaam shuru, pakki");
 //aronno what imma do with dis stuff, but i got to, 'cause i need "Dolla DOlla"
