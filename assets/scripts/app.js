77d2064035a8042ab6b717955fe2508f76dd81e8
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-02-23 01:30:52 +0500

    2nd

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,6 @@ const defaultConstant = 0;
 let currentResult = defaultConstant;
 
 currentResult = defaultConstant + 10;
-let calculationDescription = defaultConstant + ' currentResult + 10';
+let calculationDescription = defaultConstant + "currentResult" currentResult + 10';
 
 outputResult(currentResult, calculationDescription);
